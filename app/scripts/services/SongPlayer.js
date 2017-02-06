c86b8ee3c96add636865ce259d77842a24c6c2e4
kurtwayne <kurtwgardner@gmail.com>
2017-02-06 16:04:06 -0700

    Add new private function to play current song

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -24,11 +24,20 @@
             currentSong = song;
         };
         
+        /**
+        * @function playSong
+        * @desc Plays currentBuzzObject and sets current song object to true
+        * @param {Object} song
+        */        
+        var playSong = function(song) {
+            setSong(song);
+            currentBuzzObject.play();
+            song.playing = true;
+        };
+        
         SongPlayer.play = function(song) {
             if (currentSong !== song) {
-                setSong(song);
-                currentBuzzObject.play();
-                song.playing = true;
+                playSong(song);
             } else if (currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
                     currentBuzzObject.play();
